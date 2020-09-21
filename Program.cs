eaa3e8f9c4583897c8b06ac8163c3d83fae6959d
Wiktor Artymowicz <wiktor.artymowicz44@gmail.com>
2020-09-21 21:26:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Dynamic;
+using System.Threading.Tasks;
+using GUSService;
+
+namespace GabosZadanie2WebClientGus
+{
+    class Program
+    {
+        public static void Main()
+        { 
+            UslugaBIRzewnPublClient uslugaBIRzewnPublClient = new UslugaBIRzewnPublClient();
+            uslugaBIRzewnPublClient.ZalogujAsync("abcde12345abcde12345");
+            Task <DaneSzukajPodmiotyResponse> response = uslugaBIRzewnPublClient.DaneSzukajPodmiotyAsync(new ParametryWyszukiwania { Nip = "6452521870" });
+
+            Console.WriteLine(response.Result.DaneSzukajPodmiotyResult);
+        }
+    }
+}
